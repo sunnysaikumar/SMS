b80a1d57991804d2b167f9dbc5b5fdfea4a63f72
challagundla pv sai kumar <sunnysaikumar19@gmail.com>
2017-11-09 21:56:52 +0530

    Update full_details.php

--- admin/full_details.php
+++ admin/full_details.php
@@ -1,4 +1,4 @@
-<!-- Full Details -->
+
     <div class="modal fade" id="detail<?php echo $sqrow['salesid']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
