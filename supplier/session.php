6afec21919d93148fdf5778eab62339bc0f8f370
challagundla pv sai kumar <sunnysaikumar19@gmail.com>
2017-11-25 13:51:03 +0530

    Update session.php

--- supplier/session.php
+++ supplier/session.php
@@ -9,5 +9,8 @@
 	include('../conn.php');
 
 	$sq=mysqli_query($conn,"select * from `user` where userid='".$_SESSION['id']."'");
+       $srow=mysqli_fetch_array($sq);
+	
+	$user=$srow['username'];
 	
 ?>
